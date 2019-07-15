a8a228cf1be1dfa138590c68e8cbeb587e17b90d
Ethan Nguyen <thanhnx.605@gmail.com>
2019-07-15 11:56:19 +0700

    Refine

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,46 +8,61 @@ function toggleLoading(loading) {
   $('#schedule-container').empty();
 }
 
-async function generateScheduleFromInputs() {
+async function performAsyncTask(callback) {
+  try {
+    toggleLoading(true);
+    await callback();
+    toggleLoading(false);
+  } catch (error) {
+    toggleLoading(false);
+    console.warn(error);
+    alert(error.message);
+  }
+}
+
+function generateScheduleFromInputs() {
   const userId = $('#user-data input[name=user_id]').val();
   const name = $('#user-data input[name=full_name]').val();
   const height = $('#user-data input[name=height]').val();
   const weight = $('#user-data input[name=weight]').val();
-  const weeklyCode = $('#user-data select[name=weekly_code]').val();
-  const weekNumber = $('#user-data select[name=week_number]').val();
-  const checksum = window.VSG.computeChecksum(userId, height, weight);
+
+  if (_.isEmpty(userId) || _.isEmpty(name) || _.isEmpty(height) || _.isEmpty(weight))
+    throw new Error('Thiếu thông tin của khách hàng!');
+
   const userInfo = {
     userId,
     name,
     height,
     weight,
   };
+  const weeklyCode = $('#user-data select[name=weekly_code]').val();
+  const weekNumber = $('#user-data select[name=week_number]').val();
   const weeklyData = _.find(window.VSG.WEEKLY_SCHEDULES, { code: weeklyCode });
-  const schedule = await VSG.renderWeeklySchedule(weeklyData, weekNumber, userInfo);
+  const schedule = VSG.renderWeeklySchedule(weeklyData, weekNumber, userInfo);
 
+  const checksum = window.VSG.computeChecksum(userId, height, weight);
   return { checksum, schedule, userId };
 }
 
-function downloadSchedule(schedule, checksum, userId) {
+async function onDownloadSchedules() {
+  const { checksum, schedule, userId } = await generateScheduleFromInputs();
+
   const scheduleBlob = new Blob([schedule], { type: 'text/html;charset=utf-8' });
-  const anchor = document.createElement('a', { id: 'schedule-download' });
+  const anchor = document.createElement('a', { id: 'schedules-downloader' });
   anchor.href = URL.createObjectURL(scheduleBlob);
   anchor.download = `${userId}_${checksum.substring(checksum.length - 6)}.html`;
   anchor.click();
 }
 
-async function handleInputData() {
-  try {
-    toggleLoading(true);
+async function onShowSchedules() {
+  const { checksum, schedule, userId } = await generateScheduleFromInputs();
+  $('#schedules-wrapper').html(schedule);
+}
 
-    const { checksum, schedule, userId } = await generateScheduleFromInputs();
+function downloadSchedules() {
+  performAsyncTask(onDownloadSchedules);
+}
 
-    toggleLoading(false);
-    // downloadSchedule(schedule, checksum, userId);
-    $('#schedule-wrapper').html(schedule);
-  } catch (error) {
-    toggleLoading(false);
-    console.debug(error);
-    alert(error.message);
-  }
+function showSchedules() {
+  performAsyncTask(onShowSchedules);
 }
