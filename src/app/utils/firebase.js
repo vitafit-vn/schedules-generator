a1ede846237f3ef22c43a68169ce6710f95afed2
Ethan Nguyen <thanhnx.605@gmail.com>
2019-07-20 17:03:37 +0700

    Remove unused files & add custom babel plugins

---
+++ src/app/utils/firebase.js
@@ -0,0 +1,22 @@
+/*
+
+async function authWithGoogle() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  await firebase.auth().signInWithPopup(provider);
+}
+
+function initializeFirebase() {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCteDJ-lA8MnwQkwvwAixaEaV84fDjsBKc",
+    authDomain: "vitafit-tools.firebaseapp.com",
+    databaseURL: "https://vitafit-tools.firebaseio.com",
+    projectId: "vitafit-tools",
+    storageBucket: "",
+    messagingSenderId: "394067361112",
+    appId: "1:394067361112:web:22b393bb5843d155"
+  });
+
+  firebase.auth().languageCode = 'vi';
+}
+
+*/
