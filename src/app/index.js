8949354bd5c3c80e917e4dd09eb0897526f0f717
Ethan Nguyen <thanhnx.605@gmail.com>
2019-07-26 17:11:32 +0700

    Refine

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,4 @@
 import { render } from 'preact';
 import App from './pages';
 
-/* eslint-disable */
-Object.assign(window, {
-  LZString: require('lz-string'),
-});
-/* eslint-enable */
-
 render(<App />, document.body);
