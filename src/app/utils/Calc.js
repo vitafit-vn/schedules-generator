4044fb5b606c7225119aa4236e3ad62c1d48949f
Ethan Nguyen <thanhnx.605@gmail.com>
2019-07-29 18:32:57 +0700

    Refine

--- src/app/utils/Calc.js
+++ src/app/utils/Calc.js
@@ -13,39 +13,47 @@ export default class Calc {
     this.data = { abs, activityRate, gender, target, weight };
   }
 
+  updateData = ({ abs, activityRate, gender, target, weight }) => {
+    delete this.data;
+    this.data = { abs, activityRate, gender, target, weight };
+  };
+
   get fatPercentage() {
+    const { abs, gender, weight } = this.data;
+
     const absInInch = converters
-      .length(this.data.abs)
+      .length(abs)
       .from('cm')
       .to('in').value;
 
     const weightInPound = converters
-      .mass(this.data.weight)
+      .mass(weight)
       .from('kg')
       .to('lb').value;
 
-    if (this.data.gender === GENDERS_MAPPING.MALE)
-      return (-98.42 + 4.15 * absInInch - 0.082 * weightInPound) / weightInPound;
+    const prefix = gender === GENDERS_MAPPING.MALE ? -98.42 + 4.15 * absInInch : -76.76 + 4.15 * absInInch;
 
-    return (-76.76 + 4.15 * absInInch - 0.082 * weightInPound) / weightInPound;
+    return (prefix - 0.082 * weightInPound) / weightInPound;
   }
 
   get lbm() {
-    const fatPercent = this.fatPercentage;
-    return (1 - fatPercent) * this.data.weight;
+    return (1 - this.fatPercentage) * this.data.weight;
   }
 
   get bmr() {
     return 370 + 21.6 * this.lbm;
   }
 
-  get tdee() {
-    const adjustRate = this.data.activityRate || 1.55;
-    return adjustRate * this.bmr;
+  get currentTdee() {
+    return this.data.activityRate * this.bmr;
+  }
+
+  get targetTdee() {
+    return 1.55 * this.bmr;
   }
 
   get caloriesIn() {
-    return this.tdee + TDEE_ADJUSTMENTS[this.data.target];
+    return this.targetTdee + TDEE_ADJUSTMENTS[this.data.target];
   }
 
   get macro() {
@@ -56,47 +64,3 @@ export default class Calc {
     return { carb, fat, protein };
   }
 }
-
-// export function fatPercentage({ abs, gender, weight }) {
-//   const absInInch = converters
-//     .length(abs)
-//     .from('cm')
-//     .to('in').value;
-
-//   const weightInPound = converters
-//     .mass(weight)
-//     .from('kg')
-//     .to('lb').value;
-
-//   if (gender === GENDERS_MAPPING.MALE) return (-98.42 + 4.15 * absInInch - 0.082 * weightInPound) / weightInPound;
-//   return (-76.76 + 4.15 * absInInch - 0.082 * weightInPound) / weightInPound;
-// }
-
-// export function lbm({ abs, gender, weight }) {
-//   const fatPercent = fatPercentage({ abs, gender, weight });
-//   return (1 - fatPercent) * weight;
-// }
-
-// export function bmr({ abs, gender, weight }) {
-//   const lbmValue = lbm({ abs, gender, weight });
-//   return 370 + 21.6 * lbmValue;
-// }
-
-// export function tdee({ abs, activityRate, gender, weight }) {
-//   const adjustRate = activityRate || 1.55;
-//   const bmrValue = bmr({ abs, gender, weight });
-//   return adjustRate * bmrValue;
-// }
-
-// export function caloriesIn({ abs, gender, target, weight }) {
-//   const targetTdee = tdee({ abs, gender, weight });
-//   return targetTdee + TDEE_ADJUSTMENTS[target];
-// }
-
-// export function macro({ abs, gender, target, weight }) {
-//   const protein = weight * 2;
-//   const caloriesInValue = caloriesIn({ abs, gender, target, weight });
-//   const fat = 0.02 * caloriesInValue;
-//   const carb = (caloriesInValue - (protein * 4 + fat * 9)) / 4;
-//   return { carb, fat, protein };
-// }
