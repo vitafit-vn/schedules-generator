d937136f92c04114af8174fa8c3206d8c2822658
Ethan Nguyen <thanhnx.605@gmail.com>
2019-08-10 17:42:35 +0700

    Add charset meta tag

--- src/app/templates/reusables/HtmlHead.js
+++ src/app/templates/reusables/HtmlHead.js
@@ -18,6 +18,7 @@ const HtmlHead = ({ pageTitle }) => {
 
   return (
     <head>
+      <meta charset="UTF-8" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
